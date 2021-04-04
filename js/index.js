a8b6f24bb2012946d417a18dc15fcea02013a969
hw <sagekhw@gmail.com>
2021-04-04 13:31:49 +0900

    1

--- js/index.js
+++ js/index.js
@@ -76,7 +76,7 @@ function setOverlayMapTypeId() {
 
 }
 
-window.onload = function() {
-    // 페이지가 로딩된 후 실행해야 되는 코드를 추가한다.
-    getLocation();
-}
+// window.onload = function() {
+//     // 페이지가 로딩된 후 실행해야 되는 코드를 추가한다.
+//     getLocation();
+// }
