f73b36ff64e7f5c5678b0741096c14e6ec6864e1
hw <sagekhw@gmail.com>
2021-04-04 13:42:29 +0900

    1

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
-var latitude = "";
-var longitude = "";
+// var latitude = "";
+// var longitude = "";
 
-alert("hellop");
+// alert("hellop");
 
 // function getLocation() {
 //     if (navigator.geolocation) { // GPS를 지원하면
@@ -22,60 +22,60 @@ alert("hellop");
 // }
 
 
-// var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
-//     mapOption = {
-//         // center: new kakao.maps.LatLng(37.57319, 126.96658), // 지도의 중심좌표
-//         center: new kakao.maps.LatLng(latitude, longitude), // 지도의 중심좌표
-//         level: 7 // 지도의 확대 레벨
-//     };
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+    mapOption = {
+        // center: new kakao.maps.LatLng(37.57319, 126.96658), // 지도의 중심좌표
+        center: new kakao.maps.LatLng(latitude, longitude), // 지도의 중심좌표
+        level: 7 // 지도의 확대 레벨
+    };
 
-// var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
-// // 지도 타입 정보를 가지고 있을 객체입니다
-// // map.addOverlayMapTypeId 함수로 추가된 지도 타입은
-// // 가장 나중에 추가된 지도 타입이 가장 앞에 표시됩니다
-// // 이 예제에서는 지도 타입을 추가할 때 지적편집도, 지형정보, 교통정보, 자전거도로 정보 순으로 추가하므로
-// // 자전거 도로 정보가 가장 앞에 표시됩니다
-// var mapTypes = {
-//     terrain: kakao.maps.MapTypeId.TERRAIN,
-//     traffic: kakao.maps.MapTypeId.TRAFFIC,
-//     bicycle: kakao.maps.MapTypeId.BICYCLE,
-//     useDistrict: kakao.maps.MapTypeId.USE_DISTRICT
-// };
+// 지도 타입 정보를 가지고 있을 객체입니다
+// map.addOverlayMapTypeId 함수로 추가된 지도 타입은
+// 가장 나중에 추가된 지도 타입이 가장 앞에 표시됩니다
+// 이 예제에서는 지도 타입을 추가할 때 지적편집도, 지형정보, 교통정보, 자전거도로 정보 순으로 추가하므로
+// 자전거 도로 정보가 가장 앞에 표시됩니다
+var mapTypes = {
+    terrain: kakao.maps.MapTypeId.TERRAIN,
+    traffic: kakao.maps.MapTypeId.TRAFFIC,
+    bicycle: kakao.maps.MapTypeId.BICYCLE,
+    useDistrict: kakao.maps.MapTypeId.USE_DISTRICT
+};
 
-// // 체크 박스를 선택하면 호출되는 함수입니다
-// function setOverlayMapTypeId() {
-//     var chkTerrain = document.getElementById('chkTerrain'),
-//         chkTraffic = document.getElementById('chkTraffic'),
-//         chkBicycle = document.getElementById('chkBicycle'),
-//         chkUseDistrict = document.getElementById('chkUseDistrict');
+// 체크 박스를 선택하면 호출되는 함수입니다
+function setOverlayMapTypeId() {
+    var chkTerrain = document.getElementById('chkTerrain'),
+        chkTraffic = document.getElementById('chkTraffic'),
+        chkBicycle = document.getElementById('chkBicycle'),
+        chkUseDistrict = document.getElementById('chkUseDistrict');
 
-//     // 지도 타입을 제거합니다
-//     for (var type in mapTypes) {
-//         map.removeOverlayMapTypeId(mapTypes[type]);
-//     }
+    // 지도 타입을 제거합니다
+    for (var type in mapTypes) {
+        map.removeOverlayMapTypeId(mapTypes[type]);
+    }
 
-//     // 지적편집도정보 체크박스가 체크되어있으면 지도에 지적편집도정보 지도타입을 추가합니다
-//     if (chkUseDistrict.checked) {
-//         map.addOverlayMapTypeId(mapTypes.useDistrict);
-//     }
+    // 지적편집도정보 체크박스가 체크되어있으면 지도에 지적편집도정보 지도타입을 추가합니다
+    if (chkUseDistrict.checked) {
+        map.addOverlayMapTypeId(mapTypes.useDistrict);
+    }
 
-//     // 지형정보 체크박스가 체크되어있으면 지도에 지형정보 지도타입을 추가합니다
-//     if (chkTerrain.checked) {
-//         map.addOverlayMapTypeId(mapTypes.terrain);
-//     }
+    // 지형정보 체크박스가 체크되어있으면 지도에 지형정보 지도타입을 추가합니다
+    if (chkTerrain.checked) {
+        map.addOverlayMapTypeId(mapTypes.terrain);
+    }
 
-//     // 교통정보 체크박스가 체크되어있으면 지도에 교통정보 지도타입을 추가합니다
-//     if (chkTraffic.checked) {
-//         map.addOverlayMapTypeId(mapTypes.traffic);
-//     }
+    // 교통정보 체크박스가 체크되어있으면 지도에 교통정보 지도타입을 추가합니다
+    if (chkTraffic.checked) {
+        map.addOverlayMapTypeId(mapTypes.traffic);
+    }
 
-//     // 자전거도로정보 체크박스가 체크되어있으면 지도에 자전거도로정보 지도타입을 추가합니다
-//     if (chkBicycle.checked) {
-//         map.addOverlayMapTypeId(mapTypes.bicycle);
-//     }
+    // 자전거도로정보 체크박스가 체크되어있으면 지도에 자전거도로정보 지도타입을 추가합니다
+    if (chkBicycle.checked) {
+        map.addOverlayMapTypeId(mapTypes.bicycle);
+    }
 
-// }
+}
 
 // // window.onload = function() {
 // //     // 페이지가 로딩된 후 실행해야 되는 코드를 추가한다.
