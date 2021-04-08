6e6a16dcd2c0f014afee4170580ae32be0afd626
hw <sagekhw@gmail.com>
2021-04-08 20:26:51 +0900

    ssl

--- js/index.js
+++ js/index.js
@@ -1,25 +1,25 @@
-// var latitude = "";
-// var longitude = "";
+var latitude = "";
+var longitude = "";
 
-// // alert("hellop");
+// alert("hellop");
 
-// function getLocation() {
-//     if (navigator.geolocation) { // GPS를 지원하면
-//         navigator.geolocation.getCurrentPosition(function(position) {
-//             alert(position.coords.latitude + ' ' + position.coords.longitude);
-//             latitude =position.coords.latitude;
-//             longitude =position.coords.longitude;
-//         }, function(error) {
-//             console.error(error);
-//         }, {
-//             enableHighAccuracy: false,
-//             maximumAge: 0,
-//             timeout: Infinity
-//         });
-//     } else {
-//         alert('GPS를 지원하지 않습니다');
-//     }
-// }
+function getLocation() {
+    if (navigator.geolocation) { // GPS를 지원하면
+        navigator.geolocation.getCurrentPosition(function(position) {
+            alert(position.coords.latitude + ' ' + position.coords.longitude);
+            latitude =position.coords.latitude;
+            longitude =position.coords.longitude;
+        }, function(error) {
+            console.error(error);
+        }, {
+            enableHighAccuracy: false,
+            maximumAge: 0,
+            timeout: Infinity
+        });
+    } else {
+        alert('GPS를 지원하지 않습니다');
+    }
+}
 function hello(){
     alert(latitude + longitude);
 }
